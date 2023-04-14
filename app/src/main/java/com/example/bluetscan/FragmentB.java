b0cdff333dc10bce0e0f50c23153845cd140e2a6
1Mohammed01 <mohammedtla636@gmail.com>
2023-04-15 00:53:57 +0530

    mohammed settings page

--- app/src/main/java/com/example/bluetscan/FragmentB.java
+++ app/src/main/java/com/example/bluetscan/FragmentB.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class FragmentB extends Fragment {
@@ -17,5 +20,18 @@ public class FragmentB extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_b, container, false);
+
+
+        ListView listView1 = new ListView(this);
+        listView1=listView1.findViewById(R.id.my_list_view);
+        ArrayList<String> items = new ArrayList<>();
+        items.add("Item 1");
+        items.add("Item 2");
+        items.add("Item 3");
+
+        MyListAdapter adapter = new MyListAdapter(this, items);
+        listView1.setAdapter(adapter);
+
+
     }
 }
